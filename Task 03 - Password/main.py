e90069387cd9a1f3128399f6bf9624687417f708
jkoniecznyy <62672370+jkoniecznyy@users.noreply.github.com>
2021-05-20 16:18:11 +0200

    Small changes in main.py (Task 03)

--- Task 03 - Password/main.py
+++ Task 03 - Password/main.py
@@ -6,23 +6,14 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     userController = UserController('crypto', 'users')
 
-    # Co jest lepszym wyjsciem?
-    # username = input('Creating a user, please write the username')
-    # czy
-    # logging.info('Creating a user, please write the username')
-    # username = input()
-    logging.info('Creating a user, please write the username')
-    username = input()
-    logging.info('Please write the password')
-    password1 = input()
-    logging.info('Please verify the password')
-    password2 = input()
+    username = input('Creating a user, please write the username')
+    password1 = input('Please write the password')
+    password2 = input('Please verify the password')
     if password1 == password2:
         try:
             userController.createUser(username, password1)
+            print("Password successfully saved")
         except Exception as ex:
             logging.error(ex)
-        logging.info(userController.verifyPassword(username, password1))
-        logging.info(userController.verifyPassword(username, password1+'x'))
     else:
         logging.warning('Passwords dont match')
